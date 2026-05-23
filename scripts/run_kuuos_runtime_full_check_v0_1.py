9af094c2f76ca2df4b536dcea84952952ae54b28
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-23 18:39:43 +0900

    Add Qi process tensor reentry plan to runtime full check v0.1

--- scripts/run_kuuos_runtime_full_check_v0_1.py
+++ scripts/run_kuuos_runtime_full_check_v0_1.py
@@ -48,6 +48,7 @@ TEST_MODULES = [
     "tests.test_qi_process_tensor_actuator_output_fields_v0_1",
     "tests.test_qi_process_tensor_tick_scheduler_output_fields_v0_1",
     "tests.test_qi_process_tensor_closed_loop_receipt_v0_1",
+    "tests.test_qi_process_tensor_reentry_plan_v0_1",
     "tests.test_qi_process_tensor_closed_loop_receipt_daemon_result_fields_v0_1",
     "tests.test_kuuos_runtime_daemon_geometric_chain_fields_v0_1",
 ]
