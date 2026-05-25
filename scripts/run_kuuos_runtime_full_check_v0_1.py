a7e6ff56d01169f0c606e22d2208f27b5277c7f4
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-25 20:50:15 +0900

    Add Qi persistent supervisor CLI check to runtime full check v0.1

--- scripts/run_kuuos_runtime_full_check_v0_1.py
+++ scripts/run_kuuos_runtime_full_check_v0_1.py
@@ -26,6 +26,7 @@ CHECK_MODULES = [
     "scripts.check_qi_daemon_once_operator_docs_v0_1",
     "scripts.check_qi_supervised_loop_operator_cli_v0_1",
     "scripts.check_qi_controlled_loop_operator_cli_v0_1",
+    "scripts.check_qi_persistent_supervisor_operator_cli_v0_1",
 ]
 
 TEST_MODULES = [
