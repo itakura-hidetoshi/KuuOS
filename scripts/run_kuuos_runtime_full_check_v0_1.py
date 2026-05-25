a537a155cc7325d79b9affa7ca484c7ef8a78e70
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-05-26 08:06:58 +0900

    Add Qi profile validator check to runtime full check v0.1

--- scripts/run_kuuos_runtime_full_check_v0_1.py
+++ scripts/run_kuuos_runtime_full_check_v0_1.py
@@ -32,6 +32,7 @@ CHECK_MODULES = [
     "scripts.check_qi_persistent_supervisor_runbook_v0_1",
     "scripts.check_qi_persistent_supervisor_operator_e2e_v0_1",
     "scripts.check_qi_persistent_supervisor_profile_runner_v0_1",
+    "scripts.check_qi_persistent_supervisor_profile_validator_v0_1",
 ]
 
 TEST_MODULES = [
