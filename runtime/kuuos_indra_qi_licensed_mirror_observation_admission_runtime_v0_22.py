200e486eff20c7914e98789b8fc30486b7205462
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-15 17:35:39 +0900

    Add v0.22 mirror observation runtime facade

---
+++ runtime/kuuos_indra_qi_licensed_mirror_observation_admission_runtime_v0_22.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+from runtime.kuuos_indra_qi_licensed_mirror_observation_admission_core_v0_22 import BLOCKED, READY, Result, build_licensed_mirror_observation_admission
+
+__all__ = ["BLOCKED", "READY", "Result", "build_licensed_mirror_observation_admission"]
