b302cff5c569ce5498be0411d4414115f368ac24
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-18 21:35:38 +0900

    Include LearnOS v0.1 in runtime full check

--- scripts/run_kuuos_runtime_full_check_v0_1.py
+++ scripts/run_kuuos_runtime_full_check_v0_1.py
@@ -42,6 +42,7 @@ CHECK_MODULES = [
     "scripts.check_act_os_authority_bound_invocation_v0_1",
     "scripts.check_observe_os_effect_grounded_observation_v0_1",
     "scripts.check_verify_os_evidence_bound_verification_v0_1",
+    "scripts.check_learn_os_future_only_evidence_learning_v0_1",
 ]
 
 TEST_MODULES = [
