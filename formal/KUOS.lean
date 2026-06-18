e8bea56f7c66418e57ea9f493d6f2b7c9fb11e6d
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-18 15:43:50 +0900

    Register Gauge-Qi Process GraphRAG v0.2 formal surface

--- formal/KUOS.lean
+++ formal/KUOS.lean
@@ -4,3 +4,4 @@ import KUOS.OpenHorizon.AutonomousMissionCycleKernelV0_21
 import KUOS.BeliefOS.RelationalConditionalKernelV0_1
 import KUOS.BeliefOS.ContextGaugeCredalTransportV0_2
 import KUOS.GraphRAG.GaugeQiProcessGraphRAGV0_1
+import KUOS.GraphRAG.GaugeQiProcessGraphRAGV0_2
