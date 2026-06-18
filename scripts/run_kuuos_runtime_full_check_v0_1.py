11f811ce9899d4b9bc7c729356e0d37505e4c22d
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-19 06:24:05 +0900

    Include PlanOS v0.4 in runtime full check

--- scripts/run_kuuos_runtime_full_check_v0_1.py
+++ scripts/run_kuuos_runtime_full_check_v0_1.py
@@ -41,6 +41,7 @@ CHECK_MODULES = [
     "scripts.check_plan_os_replan_bound_synthesis_v0_1",
     "scripts.check_plan_os_qi_conditioned_nonmarkov_replan_v0_2",
     "scripts.check_plan_os_next_cycle_basis_compiler_adapter_v0_3",
+    "scripts.check_plan_os_closed_loop_replan_intake_adapter_v0_4",
     "scripts.check_act_os_authority_bound_invocation_v0_1",
     "scripts.check_act_os_replan_lineage_authority_envelope_v0_2",
     "scripts.check_observe_os_effect_grounded_observation_v0_1",
