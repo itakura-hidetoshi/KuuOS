bd9377672deecd1f80d8c74cd341df986d7de81a
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-19 13:23:39 +0900

    Import PlanOS v0.8 formal surface

--- formal/KUOS.lean
+++ formal/KUOS.lean
@@ -14,6 +14,7 @@ import KUOS.PlanOS.ClosedLoopReplanIntakeAdapterV0_4
 import KUOS.PlanOS.GenerationalReplanCycleDriverV0_5
 import KUOS.PlanOS.BoundedMultiGenerationSupervisorV0_6
 import KUOS.PlanOS.ExternalResumeHandoverReentryV0_7
+import KUOS.PlanOS.ReentryOwnershipContinuityV0_8
 import KUOS.ActOS.AuthorityBoundInvocationV0_1
 import KUOS.ActOS.ReplanLineageAuthorityEnvelopeV0_2
 import KUOS.ObserveOS.EffectGroundedObservationV0_1
