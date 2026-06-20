162a21b087a33324c15277ff2141867a7195ad02
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-20 09:41:32 +0900

    Add WORLD information-geometric higher gauge runtime facade v0.43

---
+++ runtime/kuuos_world_information_geometric_higher_gauge_bridge_runtime_v0_43.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from runtime.kuuos_world_information_geometric_higher_gauge_bridge_core_v0_43 import (
+    BLOCKED,
+    READY,
+    Result,
+    build_world_information_geometric_higher_gauge_bridge,
+)
+
+__all__ = [
+    "BLOCKED",
+    "READY",
+    "Result",
+    "build_world_information_geometric_higher_gauge_bridge",
+]
