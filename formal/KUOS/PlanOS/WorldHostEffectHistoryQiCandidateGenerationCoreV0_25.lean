5b87202ba76985f3e6b17ab7b3fffd403a1234e6
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-24 06:15:28 +0900

    Add PlanOS v0.25 disposition boundary

--- formal/KUOS/PlanOS/WorldHostEffectHistoryQiCandidateGenerationCoreV0_25.lean
+++ formal/KUOS/PlanOS/WorldHostEffectHistoryQiCandidateGenerationCoreV0_25.lean
@@ -4,5 +4,23 @@ import KUOS.PlanOS.VacuumExpectationHistoryQiCandidateGenerationV0_19
 namespace KUOS
 namespace PlanOS
 
+structure WorldDispositionGenerationBoundary where
+  sourceDispositionPreserved : Bool
+  governanceReviewPreserved : Bool
+  worldCommitSeparate : Bool
+  freshCommitAuthorizationRequired : Bool
+  freshCommitAuthorizationSupplied : Bool
+  atomicCommitReady : Bool
+  generatedCandidateIsWorldDisposition : Bool
+  generationResolvesWorldDisposition : Bool
+  dispositionRequired : sourceDispositionPreserved = true
+  governanceRequired : governanceReviewPreserved = true
+  separateCommitRequired : worldCommitSeparate = true
+  authorizationRequired : freshCommitAuthorizationRequired = true
+  authorizationNotSupplied : freshCommitAuthorizationSupplied = false
+  readinessForbidden : atomicCommitReady = false
+  candidateDistinctionRequired : generatedCandidateIsWorldDisposition = false
+  resolutionForbidden : generationResolvesWorldDisposition = false
+
 end PlanOS
 end KUOS
