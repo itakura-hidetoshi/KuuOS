28755c55b87dfc1fb06df1c333770b8665ced3a8
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-24 06:25:22 +0900

    Add PlanOS v0.26 constraint handoff types skeleton

---
+++ formal/KUOS/PlanOS/WorldHostEffectConstraintDecisionHandoffTypesV0_26.lean
@@ -0,0 +1,11 @@
+import Mathlib
+import KUOS.PlanOS.WorldHostEffectHistoryQiCandidateGenerationV0_25
+import KUOS.PlanOS.WorldHostEffectConstraintDecisionHandoffCoreV0_26
+
+namespace KUOS
+namespace PlanOS
+
+open WORLD ObserveOS VerifyOS LearnOS DecisionOS ActOS
+
+end PlanOS
+end KUOS
