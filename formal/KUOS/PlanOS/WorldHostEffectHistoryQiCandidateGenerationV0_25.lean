b978e0f6344797da3beab0b229f00e2427f3f117
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-24 06:17:24 +0900

    Add PlanOS v0.25 generation theorem skeleton

---
+++ formal/KUOS/PlanOS/WorldHostEffectHistoryQiCandidateGenerationV0_25.lean
@@ -0,0 +1,10 @@
+import Mathlib
+import KUOS.PlanOS.WorldHostEffectHistoryQiCandidateGenerationTypesV0_25
+
+namespace KUOS
+namespace PlanOS
+
+open WORLD ObserveOS VerifyOS LearnOS DecisionOS ActOS
+
+end PlanOS
+end KUOS
