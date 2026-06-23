b376e32e06555208d43020010f2f053fef81a842
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-24 06:16:05 +0900

    Add PlanOS v0.25 generation types skeleton

---
+++ formal/KUOS/PlanOS/WorldHostEffectHistoryQiCandidateGenerationTypesV0_25.lean
@@ -0,0 +1,11 @@
+import Mathlib
+import KUOS.PlanOS.WorldHostEffectLearningReplanIntakeV0_24
+import KUOS.PlanOS.WorldHostEffectHistoryQiCandidateGenerationCoreV0_25
+
+namespace KUOS
+namespace PlanOS
+
+open WORLD ObserveOS VerifyOS LearnOS DecisionOS ActOS
+
+end PlanOS
+end KUOS
