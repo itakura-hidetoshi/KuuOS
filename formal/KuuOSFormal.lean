177e7fa38f430042a30f1bdc457c90eefa4f842a
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-24 20:27:46 +0900

    Register WORLD v0.56 aggregate formal import

--- formal/KuuOSFormal.lean
+++ formal/KuuOSFormal.lean
@@ -15,3 +15,4 @@ import KUOS.ObserveOS.WorldHostEffectObservationReceiptV0_4
 import KUOS.WORLD.VacuumExpectationObserveOSCommitVerifyHandoffBridgeV0_53
 import KUOS.WORLD.KuuVacuumCentralReferenceStateBridgeV0_54
 import KUOS.WORLD.KuuVacuumInformationGeometryBridgeV0_55
+import KUOS.WORLD.InfiniteDiagonalArakiHessianBridgeV0_56
