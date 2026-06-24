28a8c9116b9b2537d71b049b20e8b39fcd44a59a
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-24 17:53:52 +0900

    Register WORLD v0.50 formal module

--- formal/KUOS.lean
+++ formal/KUOS.lean
@@ -88,3 +88,4 @@ import KUOS.WORLD.QuantumGeodesicMirrorDescentFreeEnergyBridgeV0_46
 import KUOS.WORLD.QuantumGradientJKOEntropyProductionBridgeV0_47
 import KUOS.WORLD.QuantumLogSobolevContractivityMixingBridgeV0_48
 import KUOS.WORLD.KuuVacuumOSHilbertCompletionBridgeV0_49
+import KUOS.WORLD.KuuVacuumCentralReferenceStateBridgeV0_50
