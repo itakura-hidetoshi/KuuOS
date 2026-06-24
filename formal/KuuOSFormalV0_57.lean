5f4a70c07f7ea751729a3cd5d016e7a060cd0c09
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-25 08:52:29 +0900

    Register closed Tomita involution bridge

--- formal/KuuOSFormalV0_57.lean
+++ formal/KuuOSFormalV0_57.lean
@@ -2,3 +2,4 @@ import KUOS.WORLD.TomitaGraphCoreV0_57
 import KUOS.WORLD.TomitaRealLinearPMapBridgeV0_57
 import KUOS.WORLD.TomitaRealGraphConstructionV0_57
 import KUOS.WORLD.TomitaClosedConjugateLinearBridgeV0_57
+import KUOS.WORLD.TomitaClosedInvolutionBridgeV0_57
