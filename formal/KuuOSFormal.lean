a0b9b1797de519a433c7818ef976b415e18a08c5
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-27 21:26:54 +0900

    Import v0.64 evaluation core

--- formal/KuuOSFormal.lean
+++ formal/KuuOSFormal.lean
@@ -24,3 +24,4 @@ import KUOS.WORLD.KuuOSOSAssociatedGaugeFieldsV0_61
 import KUOS.WORLD.KuuOSConnectionCandidateCoreV0_62
 import KUOS.WORLD.KuuOSConnectionCandidateGaugeInvarianceV0_62
 import KUOS.WORLD.KuuOSConnectionReviewCoreV0_63
+import KUOS.WORLD.KuuOSConnectionEvaluationCoreV0_64
