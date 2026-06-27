d890022d9100e3b1f0a0a8d350f925db039ad7d5
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-27 22:16:47 +0900

    Define v0.65 staging package invariants

--- formal/KUOS/WORLD/KuuOSConnectionStagingV0_65.lean
+++ formal/KUOS/WORLD/KuuOSConnectionStagingV0_65.lean
@@ -2,4 +2,34 @@ import KUOS.WORLD.KuuOSConnectionEvaluationCoreV0_64
 
 namespace KUOS.WORLD.KuuOSConnectionStagingV0_65
 
+structure StagingPackage where
+  packageDigestMatches : Prop
+  sourceDigestMatches : Prop
+  proposalDigestMatches : Prop
+  admissionDigestMatches : Prop
+  evaluationDigestMatches : Prop
+  candidateDigestMatches : Prop
+  rollbackDigestMatches : Prop
+  shadowNamespace : Prop
+  immutableEnvelope : Prop
+  candidateOnly : Prop
+  productionApplyDenied : Prop
+  stateWriteDenied : Prop
+  authorityWideningDenied : Prop
+
+structure StagingPackage.Valid (package : StagingPackage) : Prop where
+  packageDigestMatches : package.packageDigestMatches
+  sourceDigestMatches : package.sourceDigestMatches
+  proposalDigestMatches : package.proposalDigestMatches
+  admissionDigestMatches : package.admissionDigestMatches
+  evaluationDigestMatches : package.evaluationDigestMatches
+  candidateDigestMatches : package.candidateDigestMatches
+  rollbackDigestMatches : package.rollbackDigestMatches
+  shadowNamespace : package.shadowNamespace
+  immutableEnvelope : package.immutableEnvelope
+  candidateOnly : package.candidateOnly
+  productionApplyDenied : package.productionApplyDenied
+  stateWriteDenied : package.stateWriteDenied
+  authorityWideningDenied : package.authorityWideningDenied
+
 end KUOS.WORLD.KuuOSConnectionStagingV0_65
