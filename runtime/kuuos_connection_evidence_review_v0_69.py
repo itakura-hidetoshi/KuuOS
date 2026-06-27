f29fa25d48ceec9822099a9c540ebed1fdf11b90
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-28 00:07:13 +0900

    Restore canonical v0.69 evaluator shim

---
+++ runtime/kuuos_connection_evidence_review_v0_69.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+from runtime.kuuos_connection_evidence_review_bound_v0_69 import (
+    evaluate_external_evidence_review,
+)
+
+__all__ = ["evaluate_external_evidence_review"]
