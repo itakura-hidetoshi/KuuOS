583b27bb2de6718fe6c3919c5c7498b8b75f27ec
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-30 07:31:58 +0900

    Add strict checkpoint stability v1.04 export

---
+++ runtime/kuuos_repository_checkpoint_stability_strict_v1_04.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from runtime.kuuos_repository_checkpoint_stability_v1_04 import (
+    certify_repository_checkpoint_stability,
+    repository_checkpoint_stability_certificate_issues,
+)
+
+__all__ = [
+    "certify_repository_checkpoint_stability",
+    "repository_checkpoint_stability_certificate_issues",
+]
