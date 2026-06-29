ba3a693d6880ed00a440caeef8481b559e8ecfd5
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-30 06:33:49 +0900

    Finalize checkpoint receipt v1.03 public types

--- runtime/kuuos_repository_checkpoint_creation_receipt_types_v1_03.py
+++ runtime/kuuos_repository_checkpoint_creation_receipt_types_v1_03.py
@@ -3,6 +3,4 @@ from __future__ import annotations
 
 from runtime.v103_receipt_policy import *
 from runtime.v103_evidence_a import *
-from runtime.v103_evidence_b import *
-from runtime.v103_evidence_c import *
-from runtime.v103_receipt_record import *
+from runtime.v103_result import *
