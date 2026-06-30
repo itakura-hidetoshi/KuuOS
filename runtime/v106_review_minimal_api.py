bd20ca2daba0b8a0a4e9de5d10bbf8ca3509c53f
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-30 12:34:50 +0900

    Remove superseded minimal HIL wrapper

--- runtime/v106_review_minimal_api.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from runtime.v106_review_core import (
-    review_repository_checkpoint_discrepancy as base_review,
-)
-from runtime.v106_review_hil import minimize_human_review
-
-
-def review_repository_checkpoint_discrepancy(*args, **kwargs):
-    return minimize_human_review(base_review(*args, **kwargs))
