46c64e2ebb5294ef5c1455324d02b64d1aa45511
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-30 09:56:45 +0900

    Add strict review surface v1.06

---
+++ runtime/v106_review_strict.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from runtime.kuuos_repository_checkpoint_discrepancy_review_v1_06 import (
+    repository_checkpoint_review_record_issues,
+    review_repository_checkpoint_discrepancy,
+)
+
+__all__ = [
+    "review_repository_checkpoint_discrepancy",
+    "repository_checkpoint_review_record_issues",
+]
