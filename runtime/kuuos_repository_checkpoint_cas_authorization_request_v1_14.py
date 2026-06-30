729f89cbd2d0583dcd1433ff9cc90c536724add4
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-01 04:18:28 +0900

    Add checkpoint CAS authorization request facade v1.14

---
+++ runtime/kuuos_repository_checkpoint_cas_authorization_request_v1_14.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from runtime.v114_checkpoint_cas_authorization_request_core import (
+    build_repository_checkpoint_cas_authorization_request_policy,
+    construct_repository_checkpoint_cas_authorization_request,
+    derive_repository_checkpoint_cas_authorization_request,
+    repository_checkpoint_cas_authorization_request_issues,
+    repository_checkpoint_cas_authorization_request_policy_issues,
+    repository_checkpoint_cas_coherence_receipt_valid_for_request,
+)
+
+__all__ = [
+    "build_repository_checkpoint_cas_authorization_request_policy",
+    "repository_checkpoint_cas_authorization_request_policy_issues",
+    "repository_checkpoint_cas_coherence_receipt_valid_for_request",
+    "construct_repository_checkpoint_cas_authorization_request",
+    "derive_repository_checkpoint_cas_authorization_request",
+    "repository_checkpoint_cas_authorization_request_issues",
+]
