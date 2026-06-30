8081c33e7f7f5a3063e125fe08e9764859872815
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-01 03:40:35 +0900

    Add checkpoint CAS coherence facade v1.13

---
+++ runtime/kuuos_repository_checkpoint_cas_coherence_v1_13.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from runtime.v113_checkpoint_cas_coherence_core import (
+    construct_repository_checkpoint_cas_coherence_receipt,
+    derive_repository_checkpoint_cas_coherence_receipt,
+    repository_checkpoint_cas_coherence_receipt_issues,
+    repository_checkpoint_cas_contract_local_coherence,
+    repository_checkpoint_contract_intake_binding_exact,
+    repository_checkpoint_validated_cas_intake_local_coherence,
+)
+
+__all__ = [
+    "repository_checkpoint_cas_contract_local_coherence",
+    "repository_checkpoint_validated_cas_intake_local_coherence",
+    "repository_checkpoint_contract_intake_binding_exact",
+    "construct_repository_checkpoint_cas_coherence_receipt",
+    "derive_repository_checkpoint_cas_coherence_receipt",
+    "repository_checkpoint_cas_coherence_receipt_issues",
+]
