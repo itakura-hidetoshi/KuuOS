1042eda1de72541e71c661f99f620362e1503ffe
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-06-30 22:12:25 +0900

    Add validated checkpoint CAS intake facade v1.12

---
+++ runtime/kuuos_repository_checkpoint_validated_cas_intake_v1_12.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from runtime.v112_checkpoint_validated_cas_intake_core import (
+    build_repository_checkpoint_validated_cas_intake_policy,
+    construct_repository_checkpoint_validated_cas_intake,
+    derive_repository_checkpoint_validated_cas_intake,
+    repository_checkpoint_validated_cas_intake_issues,
+    repository_checkpoint_validated_cas_intake_policy_issues,
+)
+
+__all__ = [
+    "build_repository_checkpoint_validated_cas_intake_policy",
+    "repository_checkpoint_validated_cas_intake_policy_issues",
+    "construct_repository_checkpoint_validated_cas_intake",
+    "derive_repository_checkpoint_validated_cas_intake",
+    "repository_checkpoint_validated_cas_intake_issues",
+]
