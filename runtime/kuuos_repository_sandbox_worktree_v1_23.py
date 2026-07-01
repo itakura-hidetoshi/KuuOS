a5f2febc56ff5d54db24423954360e7c4eefdd8f
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-01 21:50:29 +0900

    Complete sandbox worktree facade v1.23

--- runtime/kuuos_repository_sandbox_worktree_v1_23.py
+++ runtime/kuuos_repository_sandbox_worktree_v1_23.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+from runtime.v123_sandbox_worktree_core import (
+    build_repository_sandbox_worktree_policy,
+    build_repository_sandbox_worktree_request,
+    execute_repository_sandbox_worktree,
+    repository_sandbox_worktree_policy_issues,
+    repository_sandbox_worktree_request_issues,
+    repository_sandbox_worktree_result_issues,
+    sandbox_directory_name,
+)
+
+__all__ = (
+    "sandbox_directory_name",
+    "build_repository_sandbox_worktree_policy",
+    "repository_sandbox_worktree_policy_issues",
+    "build_repository_sandbox_worktree_request",
+    "repository_sandbox_worktree_request_issues",
+    "execute_repository_sandbox_worktree",
+    "repository_sandbox_worktree_result_issues",
+)
