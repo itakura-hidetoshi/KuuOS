85340e97073c67357dd322d5e518792c131c6bb7
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-01 09:53:18 +0900

    Route v1.18 facade through strict effect accounting

--- runtime/kuuos_repository_checkpoint_live_ref_cas_v1_18.py
+++ runtime/kuuos_repository_checkpoint_live_ref_cas_v1_18.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-from runtime.v118_checkpoint_live_ref_cas_core import (
+from runtime.v118_checkpoint_live_ref_cas_strict import (
     build_repository_checkpoint_live_ref_cas_policy,
     build_repository_checkpoint_live_ref_cas_request,
     execute_repository_checkpoint_live_ref_cas,
+    normalize_repository_checkpoint_live_ref_cas_result,
     repository_checkpoint_live_ref_cas_policy_issues,
     repository_checkpoint_live_ref_cas_request_issues,
     repository_checkpoint_live_ref_cas_result_issues,
@@ -15,6 +16,7 @@ __all__ = [
     "repository_checkpoint_live_ref_cas_policy_issues",
     "build_repository_checkpoint_live_ref_cas_request",
     "repository_checkpoint_live_ref_cas_request_issues",
+    "normalize_repository_checkpoint_live_ref_cas_result",
     "execute_repository_checkpoint_live_ref_cas",
     "repository_checkpoint_live_ref_cas_result_issues",
 ]
