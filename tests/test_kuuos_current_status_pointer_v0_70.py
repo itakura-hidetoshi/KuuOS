c41eaedaaeea90fe8e6ff63110fb3a3e76170cb2
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-05 19:15:24 +0900

    Assert current pointer uses v0.70 root

--- tests/test_kuuos_current_status_pointer_v0_70.py
+++ tests/test_kuuos_current_status_pointer_v0_70.py
@@ -23,7 +23,7 @@ class KuuOSCurrentStatusPointerV070Test(unittest.TestCase):
     def test_pointer_targets_current_index(self):
         pointer = load_pointer()
         self.assertEqual("status/kuuos_status_index_v0_69.json", pointer["current_status_index"])
-        self.assertEqual("kuuos_current_root_sequence_v0_69", pointer["current_root_sequence"])
+        self.assertEqual("kuuos_current_root_sequence_v0_70", pointer["current_root_sequence"])
         self.assertEqual("current_pointer_not_authority_grant", pointer["authority_boundary"])
 
 
