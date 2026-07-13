6941e5bcd823b7b6c04875bbe401177703aeb139
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-13 17:18:14 +0900

    Add PlanOS v1.20 support facade

---
+++ runtime/kuuos_planos_finite_wasserstein_frechet_barycenter_dispersion_certificate_support_v0_1.py
@@ -0,0 +1,2 @@
+from runtime.kuuos_planos_finite_wasserstein_frechet_barycenter_dispersion_algebra_support_v0_1 import *  # noqa: F401,F403
+from runtime.kuuos_planos_finite_wasserstein_frechet_barycenter_dispersion_schema_support_v0_1 import *  # noqa: F401,F403
