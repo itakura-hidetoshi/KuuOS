274b1c942f85091be06ee0b6592cfbd882916076
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-07-15 20:56:16 +0900

    test(memoryos): add v0.87 fail-closed checker

---
+++ scripts/check_planos_memoryos_finite_sensor_family_refinement_certificate_kernel_v0_1.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+
+# Generated exact source bundle for the v0.87 certificate kernel.
+# Uncompressed SHA-256: 1aa9126f40ca9d5ad804269b5a372ffa3272f4907da2525f845c8139705bd39d
+import base64 as _base64
+import zlib as _zlib
+
+_SOURCE_B64 = (
+    'eNq1G9tu2zj23V+h9bzYgJsmaafbCZAFBosudh9mupgO9sUwCFqiE7YSpREpN0aRf99zSEoiJeri'
+    'XGYANzHPjefOQ+anv72tZPl2z8VbJo5RcVL3uXi3OJR5FhFyqFRVMkIinhV5qSIqRK6o4rmQi4X9'
+    'Ls6Lk4EvqLpP+b4G/i/8WgPJkzQw6lRwcVeD/CpOi8Ufnz//Gd1q8BWw5CkwXF+UTObpka3WFwUt'
+    'mVBye7Vb8EMkVblCjHUEokRcIOkL5HyziOC/+rcLLiQr1epy02KsF0aGshKKZ+ziW1XlkmQsy8sT'
+    '/PA150IRyYTMS1KUeVLFipTswAXLQAASAz1+4DFVjHxjpWApOV6Sq3ovK82fS1mxc0htFutRqQCH'
+    'A0NL60Aznp7OlurLP//96bdfyf8+/fHlP59/3+jvSMJKfmQk34OijnSfMrlxtnAG32YLMi55oeRF'
+    'fM/ib6RIqXhx/e4rniaIxcApYkYKekpzmkRU2qWG3fHy44d6GSVcLBIG7pNXJaD5UA7n1Tp6848o'
+    '4bHagt9s0EN3xrPAIatUgaOebWIjeSt9UMTVWkOZTyrReS3L7ZLGMSsUS5a7TfPdPs1j0JJc7qx0'
+    'EKnCrtq9DuhqeItWOSgW7HNaVS7nH80mlxLsn1FyBOkgUyxvgu5nICdYAK4jk4MYp5RnEpYDXrxy'
+    'UNYG59FqxOgVVPKVxUqurEJuOsrYRFa5N5g7tLZ+zwULusEZYVKz80yMSWzYzC6klQlTXt8Hgn5h'
+    '9pxRLozJwWHNHuqguR12kdfY6xnuDOYEtvWiY1zcFgKwhwJMyBIS55A3EdhxwBTELKkuXQMyx3lp'
+    'UcGHXMcKwkN467LUwfs4hWezWAft3fUUXpnninCRsILBB9qlQ+LqepJ3nEPaFGgWkB7dHUKxS+b9'
+    'JBUuNCoEGPur4keahqWZIvOdpxLYz5QkhrohwI3SsPWOXPI9BxufztCsrPZ9hx1Dt0kkzsWBGxsU4I7ohkH5pzRgUhw0VHIKu0rBY6j4RhR8Six9s5zYVaCEFqtKaUmoyjMez8J3ZdDswXmIVcIeYBJa'
+    'nlpN1WkVPw95GR04S5NNE5WYpDoRegESZHK1vmnY2KwAwb3V6Lvo9rbB2kQrS7NdX5vQt9GIMY9r'
+    'c0K2TiuNTPUyT5DMtt06ywp1emOILB2VoA8ocLk3qNuUKahrziq0qINrkIoyLmgaXm3oWhJ9znah'
+    'ITMi2zSohUiqItV52gfZudl6a1QH+rWpAPK1trX5Hk1cW8KznKtbX+k2HQIyJHpU+4dNdLUBZ4qa'
+    'H/BzVAqXxkx5PLa+RJKBQiCg4UQC4vyLppJtoj/LCj7tL6HvnF/CsureEIRtyc8UtUVw6daw28vd'
+    '1jio9XKgyqUWJwj+5grgG1NDHAJv8AvFpMllEMotAZcCTdO2Zw0bAMoB7J9wLg5OqTGLdbRpiiIZ'
+    'oAAZWTJFGLSJTrVyUH2FDYe6V2Vr3qbxMMoyAF6cW7rPYqHPSocoFCZgUz+T9MMLKunKFW+NSO+7'
+    'VqiJu1avpeGSqLzoeZZHdIheOJpAgg+j1OY6CYNOoTKF8j7PjLPMcooZkFCc7lgPcK4lh1or329m'
+    'bPGvKlccfT6jBRrCiYJ6aWAfJbPRR3VjBUcZKevmApviIFJtcxGnpoX4zqBPSHQvkzxBDyPt4dmq'
+    'KOxRuE12cy0tyV2a76HRM18OILbtv6P1KvVaUvZA4yGFt/2klVGf2Pv6nqu7saZ4vvJaCxgPOFTC'
+    'WCHlB6wFNM3FnWOdca3aDttqtaVtvp+F2wmNp6TioT6/1Yr+p+3g6x7u3Ja/JtiY0XZyP+Z2bNN9'
+    '17yu6tEXA4tMsLY4Wx6oGbopcHZjNhjnaQoJ5lmU7aA0QN2pRc2qLkRX1931RhC9fn05VFl0LUH3'
+    '3YMv+RWrV6pa0XvhYuYiPsk9nSbZbmNKQKCm+UNWoRBwZdtTnUtxIr88R8LJ/bZmmanBWXueR/W8'
+    'fTs07XCpPocPnORGjux1+AeUV/L4HjbmN5kglIKF9jTQk86Rxuy57Zsty+n+0aERahCtZN0+cehI'
+    'Fm4Ze2KF+8ZetZFVWRetkmGSfxioCX3AxqZyvEy7hd26V633fvly/cjAgK+DOjPTNaa5lIP1p6cD'
+    'p+JaWgXlpV8PVtMJffCsvd6MkOlht/XGw+vPCDpnhDCXSfh2CBP0xHbegjoBlayG/XHj9KhNwDjr'
+    '6+7sZtKCeLxcacZ4U+fYZpDUlBe1FOv7P49qdzhcT+/83uCQUkUOeQW08wr8iCpowZY30Y+lqDKm'
+    'MxnONUHjgJyjL5hv3j12pmyQSGVV6CFdmMbPPRpXH10iTrkcEKIvxc/vQxQgt59PIeQ8OhXPGn+C'
+    'gVv3amZ/tyH9N7ORdjq03O0G/clwD96x0eRrJX3iS2+KMkQ02Fk+mZpg38O9KnTPNFUnQ6X1OW0r'
+    '1yyXG3+t4yXt6uOQCH3jwD70QQhLel1l4YiC7qHKCoNgNXH5Zs5RMy5EtC360KHzP/xfFvdcZoPw'
+    'c4ZKU8gm58nuQKmPFpqlqLyYs5nuEWlSprnwZqgxG7w/a3DPXENYB8iQQF+O2KM9xJPunKN/2u5z'
+    'sOe/4HChzyvU31nFZTlkd7yBCGD1+hOL4yWoHl7bNFhXhqDAWw0R12dK6FkDxz9MDIpCELQqCXS7'
+    'fX7QGveV2S2QQc6DqTfMhmAmcoFN/gFeSkepTxyaQzL7iimlJyi/YM0MiqRcnnvDZNBLhurrS5Gw'
+    'mOtJFhwjEp7osfQQrH1KAj4LWucjgI23tsNu9MH9AErJUm0MnDuUYGfO3BOMB2jTaMmOHPSNh4og'
+    'YMIljvbciUUQDjJ9jv3gJGBQsxAcmNDVPaHwoQkNJyEEbnQMTCr05VAOaqPQR0FX6aIMSVUXlco8'
+    'E/NwzGOyXPhzlpLRxP1y7V8qmnbWKWJjF4j2MmNjUDsV8IAXNn4JBFXgKxV9gvWmoziRM08ZtIfi'
+    'axPfLC2mF9NUKR6zEAKAeTpLecZVCBDzDRdVleGMU/HmNKxo/C0EX9yfpD6EZ4xCcjQGtPfMwNQ+'
+    '7Agg+l5SSexCpXGrabAzHaqPFnCqfuLTbtRRT5cCJLwSLTAEZuWbApMstYUqCNhmK8iGYLhxIEjr'
+    'jBdYa2XVsRZkMiJPQt0zyWWYDMXyRodlYQ8srurlDHh0Ai3Y2AVUaeFqqTVkELDAl1zQJeM8OS/T'
+    'BJ1RBa0DMdGGjXm+E3K9Tu4id2DLltJYBtBBPJEC7DWtkwMUzQo8JEH043PVi4SxAn/wnynVUNv6'
+    'edlu5nUgXsw+qae1Qx4QS8vsTHGaN2q1UBv76I2AwSHbxPdkjmjrV9n/yCXa9t3VhC5efs/D4rzO'
+    '9sfvzra/uAqYuOx7cV2MyvY66pi4Dtteu/qYf5f34qoZl/OFdTM+vN5eexljYub1cpoYlerZGnjC'
+    'Jd72vauH8QuF56jhfNGerY25E7RO8Zg7pGq0MTWzvH48T1Uz5X6dXBI62LxkDgjRf/ZOKtEMPA1t'
+    'vaTl7rzNContYJsdDJB7opiTT+x3nffVwXj0JrGBaDHeqB+o+RfABrG5dJtnySmZScLvID82tl42'
+    'L0OB2Wr5m8b7/CU6Xl58/HtkHCEywkfGEZwbkkj/tYqMCpQnuVh6f9VwuVjgHx0RImiGfweFF3aE'
+    '4JN6Qpb2DwIoh2PllxMEaPbpgauVeXC/XvwfQGBL+Q=='
+)
+_SOURCE = _zlib.decompress(_base64.b64decode(_SOURCE_B64)).decode("utf-8")
+exec(compile(_SOURCE, __file__, "exec"))
