b9aa4d8489aac79d0a59fee1f86f73e31d5415e3
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-09-11 09:16:13 +0900

    Align history transport receiver extensions

--- formal/KUOS/DependentOriginationHistorySensitiveTransportV0_5.lean
+++ formal/KUOS/DependentOriginationHistorySensitiveTransportV0_5.lean
@@ -5,7 +5,6 @@ namespace KUOS.DependentOriginationHistorySensitiveTransportV0_5
 
 open KUOS.DependentOriginationFunctorialTransportV0_1
 open KUOS.DependentOriginationExponentialGapTransportV0_2
-open KUOS.DependentOriginationLinearTransferConnectedReadoutV0_3
 open KUOS.DependentOriginationFiniteTransferWordV0_4
 
 universe u v w
@@ -133,6 +132,8 @@ theorem not_genuinelyHistorySensitive_of_factorization
 
 end HistoryTransport
 
+end KUOS.DependentOriginationHistorySensitiveTransportV0_5
+
 /-!
 ## The v0.4 finite transfer word as the factorizable specialization
 
@@ -140,6 +141,13 @@ The existing linear positive-time transfer word is embedded into the new parent
 history structure.  Its total-time collapse is then re-expressed as an explicit
 `TotalTimeFactorization` certificate.
 -/
+
+namespace KUOS.DependentOriginationExponentialGapTransportV0_2
+
+open KUOS.DependentOriginationFunctorialTransportV0_1
+open KUOS.DependentOriginationHistorySensitiveTransportV0_5
+
+universe u v w
 
 namespace ExponentiallyGappedVacuumTransport
 
@@ -197,4 +205,4 @@ end LinearTransferRealization
 
 end ExponentiallyGappedVacuumTransport
 
-end KUOS.DependentOriginationHistorySensitiveTransportV0_5
+end KUOS.DependentOriginationExponentialGapTransportV0_2
