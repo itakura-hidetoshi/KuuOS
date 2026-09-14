d6e128313e26ab32c28c1254123188890270bee0
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-09-15 08:32:51 +0900

    Normalize composite identity naturality in v2.43

--- formal/KUOS/DependentOriginationDiscreteBaseObstructionEliminationV2_43.lean
+++ formal/KUOS/DependentOriginationDiscreteBaseObstructionEliminationV2_43.lean
@@ -89,7 +89,8 @@ theorem storedV2_18TriangleIsModificationNatural_of_discrete
           K.comparison).naturality (𝟙 _)).hom ≫
         (storedV2_18ComparisonComponentIso (W := W) alpha _).hom ▷
           R.map (𝟙 _)
-  rw [CategoryTheory.Pseudofunctor.StrongTrans.comp_naturality_hom]
+  rw [CategoryTheory.Pseudofunctor.StrongTrans.naturality_id_hom
+    (restrictHigherLocalizedStrongTrans (W := W) alpha.hom ≫ K.comparison) _]
   apply Cat.Hom₂.ext
   ext Z
   simp [CategoryTheory.Pseudofunctor.StrongTrans.naturality_id_hom]
