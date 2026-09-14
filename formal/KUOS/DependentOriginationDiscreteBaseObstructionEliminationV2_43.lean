5cd2693a584d399bfd4d394d2fc1f4a3650d8502
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-09-15 07:37:02 +0900

    Use reassociated NatTrans naturality in v2.43

--- formal/KUOS/DependentOriginationDiscreteBaseObstructionEliminationV2_43.lean
+++ formal/KUOS/DependentOriginationDiscreteBaseObstructionEliminationV2_43.lean
@@ -92,14 +92,11 @@ theorem storedV2_18TriangleIsModificationNatural_of_discrete
   apply Cat.Hom₂.ext
   ext Z
   simp [CategoryTheory.Pseudofunctor.StrongTrans.naturality_id_hom]
-  conv_lhs =>
-    rw [← Category.assoc]
-    erw [←
-      (storedV2_18ComparisonComponentIso (W := W) alpha _).hom.toNatTrans.naturality
-        (((restrictHigherLocalizedSystem W H.lift).mapId _).hom.toNatTrans.app Z)]
-    rw [Category.assoc]
-    rw [(R.mapId _).inv.toNatTrans.naturality
-      ((storedV2_18ComparisonComponentIso (W := W) alpha _).hom.toNatTrans.app Z)]
+  erw [←
+    (storedV2_18ComparisonComponentIso (W := W) alpha _).hom.toNatTrans.naturality_assoc
+      (((restrictHigherLocalizedSystem W H.lift).mapId _).hom.toNatTrans.app Z)]
+  erw [(R.mapId _).inv.toNatTrans.naturality
+    ((storedV2_18ComparisonComponentIso (W := W) alpha _).hom.toNatTrans.app Z)]
   simp
 
 /-- Uniformly, every weak factor into the chosen coherent carrier has a stored
