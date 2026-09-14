940c3b8f164ac35e49e6fcf2532aef9bba3c2eff
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-09-15 02:15:24 +0900

    Restore discrete identity simp normalization in v2.43

--- formal/KUOS/DependentOriginationDiscreteBaseObstructionEliminationV2_43.lean
+++ formal/KUOS/DependentOriginationDiscreteBaseObstructionEliminationV2_43.lean
@@ -89,8 +89,7 @@ theorem storedV2_18TriangleIsModificationNatural_of_discrete
           K.comparison).naturality (𝟙 _)).hom ≫
         (storedV2_18ComparisonComponentIso (W := W) alpha _).hom ▷
           R.map (𝟙 _)
-  simp only [CategoryTheory.Pseudofunctor.StrongTrans.naturality_id_hom]
-  bicategory
+  simp
 
 /-- Uniformly, every weak factor into the chosen coherent carrier has a stored
 triangle that is already modification-natural. -/
