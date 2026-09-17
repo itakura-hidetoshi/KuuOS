6ba49b4439cb747536a3f21d3b00c31d78ea22db
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-09-18 08:12:09 +0900

    Use local recursive signature for generated localization erasure

--- formal/KUOS/DependentOriginationGeneratedLocalizationHolonomyV2_68.lean
+++ formal/KUOS/DependentOriginationGeneratedLocalizationHolonomyV2_68.lean
@@ -275,11 +275,11 @@ def generatedLocalization2CellToV267
       LocalizationRelation2Cell.refl p
   | .symm α =>
       LocalizationRelation2Cell.symm
-        (generatedLocalization2CellToV267 (W := W) α)
+        (generatedLocalization2CellToV267 α)
   | .trans α β =>
       LocalizationRelation2Cell.trans
-        (generatedLocalization2CellToV267 (W := W) α)
-        (generatedLocalization2CellToV267 (W := W) β)
+        (generatedLocalization2CellToV267 α)
+        (generatedLocalization2CellToV267 β)
 
 /-! ## Canonical recursive evaluation -/
 
