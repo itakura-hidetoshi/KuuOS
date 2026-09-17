5cc4705845b28432e6a8d66db876777cd1582903
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-09-18 06:04:29 +0900

    Prove mapIso gauge factorization at Iso level

--- formal/KUOS/DependentOriginationGaugeObstructionV2_66.lean
+++ formal/KUOS/DependentOriginationGaugeObstructionV2_66.lean
@@ -135,8 +135,7 @@ noncomputable def pointwiseChoiceGaugeBetween
     simp
   mapIsoGauge f := (L.mapIso f).symm ≪≫ L'.mapIso f
   mapIso_fac f := by
-    apply Iso.ext
-    simp
+    simpa using Iso.self_symm_id_assoc (L.mapIso f) (L'.mapIso f)
 
 /-- Gauge-equivalence of pointwise choice bundles. -/
 def PointwiseChoicesGaugeEquivalent
