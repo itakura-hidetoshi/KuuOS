6bbb45e899563f3a96ed997365b6239c504eb62d
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-09-19 17:00:54 +0900

    Reduce IsDiscrete identity coherence pointwise

--- formal/KUOS/DependentOriginationIsDiscreteBaseObstructionEliminationV2_44.lean
+++ formal/KUOS/DependentOriginationIsDiscreteBaseObstructionEliminationV2_44.lean
@@ -70,7 +70,10 @@ theorem storedV2_18TriangleIsModificationNatural_of_isDiscrete
   obtain rfl := IsDiscrete.eq_of_hom f
   have hf : f = 𝟙 X := Subsingleton.elim _ _
   rw [hf]
-  simp
+  apply Cat.Hom₂.ext
+  apply NatTrans.ext
+  funext A
+  simp [CategoryTheory.Pseudofunctor.StrongTrans.naturality_id_hom]
 
 /-- Uniform stored-triangle modification naturality on an abstract discrete
 base. -/
