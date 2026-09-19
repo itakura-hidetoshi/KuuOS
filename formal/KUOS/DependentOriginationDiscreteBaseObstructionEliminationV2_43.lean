8c039b51439178ba7c7e6c27c597188929d5becc
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-09-19 17:00:15 +0900

    Reduce discrete identity coherence pointwise

--- formal/KUOS/DependentOriginationDiscreteBaseObstructionEliminationV2_43.lean
+++ formal/KUOS/DependentOriginationDiscreteBaseObstructionEliminationV2_43.lean
@@ -89,7 +89,10 @@ theorem storedV2_18TriangleIsModificationNatural_of_discrete
           K.comparison).naturality (𝟙 _)).hom ≫
         (storedV2_18ComparisonComponentIso (W := W) alpha _).hom ▷
           R.map (𝟙 _)
-  simp [CategoryTheory.Pseudofunctor.StrongTrans.naturality_id_hom] <;> cat_disch
+  apply Cat.Hom₂.ext
+  apply NatTrans.ext
+  funext A
+  simp [CategoryTheory.Pseudofunctor.StrongTrans.naturality_id_hom]
 
 /-- Uniformly, every weak factor into the chosen coherent carrier has a stored
 triangle that is already modification-natural. -/
