ce7feb8963c1584d06d411b06bf6f55d32bd2a05
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-09-19 12:39:39 +0900

    Normalize embedded generators componentwise

--- formal/KUOS/DependentOriginationGeneratedCoherenceRoutesV2_68.lean
+++ formal/KUOS/DependentOriginationGeneratedCoherenceRoutesV2_68.lean
@@ -54,16 +54,28 @@ theorem generatedLocalization2CellEvaluationIso_ofGenerating_hom
       (generatedLocalization2CellOfGenerating W α)).hom =
       (generating2CellEvaluationIso W R D α).hom := by
   cases α <;>
-    simp [generatedLocalization2CellOfGenerating,
-      generatedLocalization2CellEvaluationIso,
-      generatedCompClosure2CellEvaluationIso,
-      freePathEvaluationWhiskerLeftIso,
-      freePathEvaluationWhiskerRightIso,
-      generating2CellEvaluationIso,
-      Iso.trans_hom, Iso.symm_hom, eqToIso.hom, eqToIso.inv,
-      Bicategory.whiskerLeftIso_hom, Bicategory.whiskerRightIso_hom,
-      Functor.map_id, Functor.map_comp,
-      Category.comp_id, Category.id_comp, Category.assoc]
+    apply Cat.Hom₂.ext <;>
+    ext A <;>
+    set_option backward.isDefEq.respectTransparency false in
+      simp [generatedLocalization2CellOfGenerating,
+        generatedLocalization2CellEvaluationIso,
+        generatedCompClosure2CellEvaluationIso,
+        freePathEvaluationWhiskerLeftIso,
+        freePathEvaluationWhiskerRightIso,
+        generating2CellEvaluationIso,
+        Iso.trans_hom, Iso.symm_hom, eqToIso.hom, eqToIso.inv,
+        Bicategory.whiskerLeftIso_hom, Bicategory.whiskerRightIso_hom,
+        Bicategory.Strict.leftUnitor_eqToIso,
+        Bicategory.Strict.rightUnitor_eqToIso,
+        Bicategory.Strict.associator_eqToIso,
+        Cat.Hom.id_toFunctor, Cat.Hom.id_obj, Cat.Hom.id_map,
+        Cat.Hom.comp_toFunctor, Cat.Hom.comp_obj, Cat.Hom.comp_map,
+        Cat.whiskerLeft_app, Cat.whiskerRight_app,
+        Cat.Hom₂.id_app, Cat.Hom₂.comp_app, Cat.eqToHom_app,
+        Functor.id_obj, Functor.id_map, Functor.comp_obj, Functor.comp_map,
+        Functor.map_id, Functor.map_comp,
+        eqToHom_map, eqToHom_trans, eqToHom_trans_assoc, eqToHom_refl,
+        Category.comp_id, Category.id_comp, Category.assoc]
 
 /-! ## Quotient associativity route -/
 
