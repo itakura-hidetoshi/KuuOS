cd918c44911afe5ff0b7a043c273abd3ef813767
Hidetoshi Itakura <hidetoshi.itakura@gmail.com>
2026-09-20 11:53:33 +0900

    Sync v2.43 proof with repaired v2.69 baseline

--- formal/KUOS/DependentOriginationDiscreteBaseObstructionEliminationV2_43.lean
+++ formal/KUOS/DependentOriginationDiscreteBaseObstructionEliminationV2_43.lean
@@ -89,7 +89,17 @@ theorem storedV2_18TriangleIsModificationNatural_of_discrete
           K.comparison).naturality (𝟙 _)).hom ≫
         (storedV2_18ComparisonComponentIso (W := W) alpha _).hom ▷
           R.map (𝟙 _)
-  simp [CategoryTheory.Pseudofunctor.StrongTrans.naturality_id_hom] <;> bicategory
+  rw [CategoryTheory.Pseudofunctor.StrongTrans.naturality_id_hom
+    (restrictHigherLocalizedStrongTrans (W := W) alpha.hom ≫ K.comparison) _]
+  apply Cat.Hom₂.ext
+  ext Z
+  simp [CategoryTheory.Pseudofunctor.StrongTrans.naturality_id_hom]
+  erw [←
+    (storedV2_18ComparisonComponentIso (W := W) alpha _).hom.toNatTrans.naturality_assoc
+      (((restrictHigherLocalizedSystem W H.lift).mapId _).hom.toNatTrans.app Z)]
+  erw [(R.mapId _).inv.toNatTrans.naturality
+    ((storedV2_18ComparisonComponentIso (W := W) alpha _).hom.toNatTrans.app Z)]
+  simp
 
 /-- Uniformly, every weak factor into the chosen coherent carrier has a stored
 triangle that is already modification-natural. -/
